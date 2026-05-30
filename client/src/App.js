904613aee661e8213174214a36b81f60de18f62b
petyonachev92 <pyo@abv.bg>
2026-05-30 16:28:58 +0300

    edited to fetch from render

--- client/src/App.js
+++ client/src/App.js
@@ -88,7 +88,7 @@ function App() {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/api/rsvp', {
+      const response = await fetch('https://wedding-website-ahot.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(sanitizedPayload),
