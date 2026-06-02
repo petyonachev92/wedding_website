79c84b3b4520eb9ada342317cfb67c14d6e53699
petyonachev92 <pyo@abv.bg>
2026-06-02 23:24:18 +0300

    fetch parameter changed

--- client/src/App.js
+++ client/src/App.js
@@ -89,7 +89,7 @@ function App() {
     };
 
     try {
-      const response = await fetch('https://petyoandvictoriawedding.site/api/rsvp', {
+      const response = await fetch('/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(sanitizedPayload),
